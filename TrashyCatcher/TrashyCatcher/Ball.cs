8c5c613848d9e52db613a899978e2df8030ee3d8
Øyvind Skaaden <oyvindskaaden@gmail.com>
2017-12-01 09:45:10 +0100

    Started New Project

---
+++ TrashyCatcher/TrashyCatcher/Ball.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrashyCatcher
+{
+    class Ball
+    {
+        int xPos;
+        int yPos;
+        int a = 8;
+
+        double dX;
+        double dY;
+
+        private PictureBox ball;
+        string imgFile = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\ball.png";
+
+
+
+        public Ball() { }
+
+        public Ball(int startX, int startY, double degrees, int force, Form form)
+        {
+            xPos = startX;
+            yPos = startY;
+
+            double phi = Math.PI * degrees / 180.0;
+            dX = Math.Cos(phi) * force;
+            dY = Math.Sin(phi) * force;
+
+            Console.WriteLine(dX + " - " + dY);
+
+            ball = new PictureBox();
+            ball.Image = Image.FromFile(imgFile);
+            ball.Size = new Size(50, 50);
+            ball.Location = new Point(xPos, yPos);
+            ball.SizeMode = PictureBoxSizeMode.Zoom;
+            ball.BackColor = Color.Transparent;
+            form.Controls.Add(ball);
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
---
+++ TrashyCatcher/TrashyCatcher/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrashyCatcher
+{
+    public partial class Spill : Form
+    {
+        public Spill()
+        {
+            InitializeComponent();
+        }
+
+        Ball ball1;
+
+        Random rnd = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ball1 = new Ball(rnd.Next(this.Width), rnd.Next(this.Height), 40, 40, this);
+        }
+    }
+}
