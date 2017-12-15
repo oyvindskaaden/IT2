9ceaf634f7ca9978b6566b94879467ae34ed23d9
Øyvind Skaaden <oyvindskaaden@gmail.com>
2017-12-15 12:14:35 +0100

    Started test porject
    Prints out the keydata

---
+++ PrintShit/PrintShit/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrintShit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyPressed(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine(e.KeyChar);
+        }
+
+        private void Pressed(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine(e.KeyCode);
+        }
+    }
+}
