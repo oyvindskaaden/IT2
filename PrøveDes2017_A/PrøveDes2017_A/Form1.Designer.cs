39db5c54d4c50318b2f8c0e655023588940b7eca
Øyvind Skaaden <oyvindskaaden@gmail.com>
2017-12-20 13:25:52 +0100

    ExamProject

---
+++ PrøveDes2017_A/PrøveDes2017_A/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace PrøveDes2017_A
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.luke20 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lbO2b = new System.Windows.Forms.Label();
+            this.lbO2a = new System.Windows.Forms.Label();
+            this.btO2 = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.lbO3c = new System.Windows.Forms.Label();
+            this.btO3c = new System.Windows.Forms.Button();
+            this.lbO3b = new System.Windows.Forms.Label();
+            this.btO3b = new System.Windows.Forms.Button();
+            this.lbO3a = new System.Windows.Forms.Label();
+            this.btO3a = new System.Windows.Forms.Button();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btO4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.luke20)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(561, 446);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackgroundImage = global::PrøveDes2017_A.Properties.Resources.jul;
+            this.tabPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage1.Controls.Add(this.luke20);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(553, 420);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "O1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // luke20
+            // 
+            this.luke20.Image = global::PrøveDes2017_A.Properties.Resources._20;
+            this.luke20.Location = new System.Drawing.Point(261, 138);
+            this.luke20.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.luke20.Name = "luke20";
+            this.luke20.Size = new System.Drawing.Size(121, 114);
+            this.luke20.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.luke20.TabIndex = 0;
+            this.luke20.TabStop = false;
+            this.luke20.Tag = "Lukket";
+            this.luke20.Click += new System.EventHandler(this.luke20_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lbO2b);
+            this.tabPage2.Controls.Add(this.lbO2a);
+            this.tabPage2.Controls.Add(this.btO2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(553, 420);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "O2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lbO2b
+            // 
+            this.lbO2b.AutoSize = true;
+            this.lbO2b.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbO2b.Location = new System.Drawing.Point(150, 210);
+            this.lbO2b.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbO2b.Name = "lbO2b";
+            this.lbO2b.Size = new System.Drawing.Size(70, 26);
+            this.lbO2b.TabIndex = 5;
+            this.lbO2b.Text = "lbO2b";
+            // 
+            // lbO2a
+            // 
+            this.lbO2a.AutoSize = true;
+            this.lbO2a.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbO2a.Location = new System.Drawing.Point(151, 166);
+            this.lbO2a.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbO2a.Name = "lbO2a";
+            this.lbO2a.Size = new System.Drawing.Size(70, 26);
+            this.lbO2a.TabIndex = 4;
+            this.lbO2a.Text = "lbO2a";
+            // 
+            // btO2
+            // 
+            this.btO2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btO2.Location = new System.Drawing.Point(155, 102);
+            this.btO2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btO2.Name = "btO2";
+            this.btO2.Size = new System.Drawing.Size(105, 44);
+            this.btO2.TabIndex = 3;
+            this.btO2.Text = "Tell";
+            this.btO2.UseVisualStyleBackColor = true;
+            this.btO2.Click += new System.EventHandler(this.btO2_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.lbO3c);
+            this.tabPage3.Controls.Add(this.btO3c);
+            this.tabPage3.Controls.Add(this.lbO3b);
+            this.tabPage3.Controls.Add(this.btO3b);
+            this.tabPage3.Controls.Add(this.lbO3a);
+            this.tabPage3.Controls.Add(this.btO3a);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(553, 420);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "O3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // lbO3c
+            // 
+            this.lbO3c.AutoSize = true;
+            this.lbO3c.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbO3c.Location = new System.Drawing.Point(305, 257);
+            this.lbO3c.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbO3c.Name = "lbO3c";
+            this.lbO3c.Size = new System.Drawing.Size(69, 26);
+            this.lbO3c.TabIndex = 13;
+            this.lbO3c.Text = "lbO3c";
+            // 
+            // btO3c
+            // 
+            this.btO3c.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btO3c.Location = new System.Drawing.Point(203, 250);
+            this.btO3c.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btO3c.Name = "btO3c";
+            this.btO3c.Size = new System.Drawing.Size(83, 38);
+            this.btO3c.TabIndex = 12;
+            this.btO3c.Text = "btO3c";
+            this.btO3c.UseVisualStyleBackColor = true;
+            this.btO3c.Click += new System.EventHandler(this.btO3c_Click);
+            // 
+            // lbO3b
+            // 
+            this.lbO3b.AutoSize = true;
+            this.lbO3b.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbO3b.Location = new System.Drawing.Point(305, 166);
+            this.lbO3b.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbO3b.Name = "lbO3b";
+            this.lbO3b.Size = new System.Drawing.Size(70, 26);
+            this.lbO3b.TabIndex = 11;
+            this.lbO3b.Text = "lbO3b";
+            // 
+            // btO3b
+            // 
+            this.btO3b.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btO3b.Location = new System.Drawing.Point(203, 159);
+            this.btO3b.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btO3b.Name = "btO3b";
+            this.btO3b.Size = new System.Drawing.Size(83, 38);
+            this.btO3b.TabIndex = 10;
+            this.btO3b.Text = "btO3b";
+            this.btO3b.UseVisualStyleBackColor = true;
+            this.btO3b.Click += new System.EventHandler(this.btO3b_Click);
+            // 
+            // lbO3a
+            // 
+            this.lbO3a.AutoSize = true;
+            this.lbO3a.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbO3a.Location = new System.Drawing.Point(305, 72);
+            this.lbO3a.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbO3a.Name = "lbO3a";
+            this.lbO3a.Size = new System.Drawing.Size(70, 26);
+            this.lbO3a.TabIndex = 9;
+            this.lbO3a.Text = "lbO3a";
+            // 
+            // btO3a
+            // 
+            this.btO3a.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btO3a.Location = new System.Drawing.Point(203, 65);
+            this.btO3a.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btO3a.Name = "btO3a";
+            this.btO3a.Size = new System.Drawing.Size(83, 38);
+            this.btO3a.TabIndex = 8;
+            this.btO3a.Text = "btO3a";
+            this.btO3a.UseVisualStyleBackColor = true;
+            this.btO3a.Click += new System.EventHandler(this.btO3a_Click);
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.richTextBox1);
+            this.tabPage5.Controls.Add(this.btO4);
+            this.tabPage5.Controls.Add(this.listBox1);
+            this.tabPage5.Controls.Add(this.textBox1);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(553, 420);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "O4";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(153, 307);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(252, 30);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // btO4
+            // 
+            this.btO4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btO4.Location = new System.Drawing.Point(153, 130);
+            this.btO4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btO4.Name = "btO4";
+            this.btO4.Size = new System.Drawing.Size(69, 44);
+            this.btO4.TabIndex = 6;
+            this.btO4.Text = "Kjør";
+            this.btO4.UseVisualStyleBackColor = true;
+            this.btO4.Click += new System.EventHandler(this.btO4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(153, 200);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(195, 54);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(153, 89);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 32);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 446);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Prøve";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.luke20)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.PictureBox luke20;
+        private System.Windows.Forms.Label lbO2b;
+        private System.Windows.Forms.Label lbO2a;
+        private System.Windows.Forms.Button btO2;
+        private System.Windows.Forms.Label lbO3c;
+        private System.Windows.Forms.Button btO3c;
+        private System.Windows.Forms.Label lbO3b;
+        private System.Windows.Forms.Button btO3b;
+        private System.Windows.Forms.Label lbO3a;
+        private System.Windows.Forms.Button btO3a;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btO4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PrøveDes2017_A/PrøveDes2017_A/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrøveDes2017_A
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        #region O1 Animasjon
+
+        private void luke20_Click(object sender, EventArgs e)
+        {
+            if ((string)luke20.Tag == "Lukket")
+            {
+                luke20.Image = Properties.Resources.God_Jul_gifanim;
+                luke20.BackColor = Color.White;
+                luke20.Tag = "Åpnet";
+            } else
+            {
+                luke20.Tag = "Lukket";
+                luke20.Image = Properties.Resources._20;
+                luke20.BackColor = Color.Transparent;
+            }
+        }
+
+        #endregion
+
+        #region O2 Array
+
+        // Chars som skal sammenliknes
+        char sisteBok = 'e';
+        char forsteBok = 'S';
+
+
+        private void btO2_Click(object sender, EventArgs e)
+        {
+            //Variabler som passer på antall ganger kriteriene i oppgaven holder
+            int ant1 = 0, ant2 = 0;
+
+            string[] steder = { "Rud", "Kolsås", "Vettre", "Skui", "Sandvia", "Hosle" };
+
+            // Sjekker siste bokstav
+            foreach (string s in steder)
+            {
+                if (s[s.Length - 1] == sisteBok)
+                    ant1++;
+            }
+
+            // Sjekker første bokstav
+            for (int i = 0; i < steder.Length; i++)
+            {
+                if (steder[i][steder[i].Length - 1] == sisteBok)
+                    ant2++;
+            }
+
+            // Printer til lables
+            lbO2a.Text = "" + ant1;
+            lbO2b.Text = "" + ant2;
+        }
+
+        #endregion
+
+        #region O3 Funksjoner
+        string a = "God";
+        string b = "Jul";
+        string c = "";
+
+        private void Hilsen()
+        {
+            c = a + " " + b;
+        }
+
+        private void Hilsen(string d)
+        {
+            c = d + " " + a + " " + b;
+        }
+
+        private decimal Gjennomsnitt(int[] t)
+        {
+            decimal s = 0;
+            int ant = 0;
+
+            foreach (int i in t)
+            {
+                s += i;
+                ant++;
+            }
+
+            return s / ant;
+        }
+
+        private void btO3a_Click(object sender, EventArgs e)
+        {
+            Hilsen();
+            lbO3a.Text = Convert.ToString(c);
+        }
+
+        private void btO3b_Click(object sender, EventArgs e)
+        {
+            Hilsen("HoHo");
+            lbO3b.Text = Convert.ToString(c);
+        }
+
+        private void btO3c_Click(object sender, EventArgs e)
+        {
+            int[] tall = {2,5,7,8,9,12};
+            lbO3c.Text = Convert.ToString(Gjennomsnitt(tall));
+        }
+
+        #endregion
+
+        #region O4 Forklar kode
+
+        // Koden under sorterer ut de oppføringene som inneholder textBox1 og legger de i listBox1. 
+        // Dersom du klikker på en av oppføringene i listBox1 vil mer info bli printet ut i richTextBox1
+
+        // Definerer den todimensjonale listen personer med personinfo, fornavn etternavn og nummer i global string variabel.
+        string[,] personer = { { "Roar", "Holmsen", "3456" }, { "Kari", "Imsen", "345699" }, { "Ola", "Jansen", "13123456" }, { "Kari", "Jeppsen", "0768556" }, { "Mari", "Ek", "987667" }, { "Per", "Faust", "998877654" } };
+        // Int for å holde styr på hvor mange oppføringer det er i listBox1 i global int variabel
+        int[] s;
+
+        // Clickevent for btO4
+        private void btO4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); // Tømmer listBox1 så vi kan starte med en tom listBox
+            s = new int[personer.Length]; // Definerer s som et int-array med lengde lik lengden av arrayet personer
+            int x = -1; // Lager lokal variabel som kun skal inkrementere når if-løkken er sann
+
+            // For-løkke som går gjennom alle navn gruppene. 
+            for (int i = 0; i < personer.GetLength(0); i++)
+            {
+                // Denne if-løkken sjekker om det som er skrevet i textbox1 er inneholdt i nummeret til en av oppføringene
+                if (personer[i, 2].Contains(textBox1.Text))
+                {
+                    // Hvis textbox1 er inneholdt i oppføringen som sjekkes vil x inkrementere og plasseringen av oppføringen er lagret i variabelen s
+                    x++;
+                    s[x] = i; // Ved plassering 'x' i 's' legg til 'i'.
+                    // Legger til oppføringen i listBox1 som inneholdt textBox1
+                    listBox1.Items.Add(personer[i, 0] + ", " + personer[i, 2]);
+                }
+            }
+        }
+
+        // Dersom en bruker klikker på en av oppføringene i listBox1 vil denne eventen skje
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Henter ut linjen du klikket på og viser oppføringen i richTextBox1 med mer info.
+            int i = s[listBox1.SelectedIndex]; // Definerer lokalvariabel og gir den verdien til plassering av oppføring i personer (trengs ikke)
+            // Linjen under legger til info i richTextBox1 ved plassering 'i'. (Her er ikke 'i' brukt men fungerer på samme måte, men mye lengre
+            richTextBox1.Text = personer[s[listBox1.SelectedIndex], 0] + ", " + personer[s[listBox1.SelectedIndex], 1] + ", " + personer[s[listBox1.SelectedIndex], 2];
+            // Linjen over kunne vært skrevet:
+            // richTextBox1.Text = personer[i, 0] + ", " + personer[i, 1] + ", " + personer[i, 2];
+        }
+
+        #endregion
+
+        #region O5 Global og lokal variabel
+
+        // Fra koden i oppgave 4 oppgir du en variabel som er global og en som er lokal.
+        // Fyll ut ditt valg i linjene under:
+
+        // Eks. på en global variabel: 
+        // int[] s;
+
+        // Eks. på en lokal variabel: 
+        // int x = -1;
+
+        #endregion
+
+    }
+}
